16b23658bd52508609c5c0b459010cdf8bc30072
Chris <aidoo.chris1@gmail.com>
2018-10-16 12:52:54 +0000

    work

---
+++ chris.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+string name;
+int age;
+int id;
+char grade;
+float score;
+int id;
+string  gender;
+int totalage=0;
+int numberofstudents = 0;
+int femaleCount = 0;
+int malecount = 0;
+int scoreCount = 0;
+double averageScore;
+double averageAge;
+
+ for( int x=0;x<5; x++)
+{
+ cout<< "Input student id ";
+ cin>> id;
+
+ cout<< "student name";
+ cine>>name;
+
+ cout<<"Student age"<<;
+ cin>>age;
+ numberofStudents++;
+ scoreCount = scoreCount + score;
+ totalage = totalage + age;
+
+ cout<< "your gender (male/female)";
+ cin>> gender;
+   if(gender == "male"){
+     maleCount++;
+   }
+   else if (gender == "female"){
+   femaleCount++;
+   }
+ cout<<"what is your score?";
+ cin>> score;
+
+}
+
+  cout<< ""
+
+}
+
+
